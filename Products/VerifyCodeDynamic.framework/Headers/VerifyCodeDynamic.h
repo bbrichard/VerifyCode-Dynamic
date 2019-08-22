bac496572f54333c1538ca52e1c921c0e8c48516
Richard <Richard@seektopser.com>
2019-08-22 17:15:00 +0800

    update

---
+++ Products/VerifyCodeDynamic.framework/Headers/VerifyCodeDynamic.h
@@ -0,0 +1,19 @@
+//
+//  VerifyCodeDynamic.h
+//  VerifyCodeDynamic
+//
+//  Created by Louis on 2019/8/22.
+//  Copyright © 2019 louis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VerifyCodeDynamic.
+FOUNDATION_EXPORT double VerifyCodeDynamicVersionNumber;
+
+//! Project version string for VerifyCodeDynamic.
+FOUNDATION_EXPORT const unsigned char VerifyCodeDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerifyCodeDynamic/PublicHeader.h>
+
+
